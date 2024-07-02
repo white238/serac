602740fc42fd07c3fb80d413582e58a0fd488fdf
Chris White <white238@llnl.gov>
2024-07-02 15:28:21 -0700

    avoid dubious ownership error

--- .github/actions/apply-style/entrypoint.sh
+++ .github/actions/apply-style/entrypoint.sh
@@ -4,6 +4,13 @@ CMAKE_ARGS=-DCMAKE_CXX_COMPILER=clang++
 CMAKE_ARGS=$CMAKE_ARGS -DENABLE_CLANGFORMAT=ON
 CMAKE_ARGS=$CMAKE_ARGS -DCLANGFORMAT_EXECUTABLE=/usr/bin/clang-format
 
+# Avoid error "fatal: detected dubious ownership in repository at '/github/workspace'"
+REPO_PATH=/github/workspace
+git config --global --add safe.directory "$REPO_PATH"
+find "$REPO_PATH" -type d | while read -r dir; do
+  git config --global --add safe.directory "$dir"
+done
+
 git submodule update --init --recursive
 
 mkdir build && cd build 
