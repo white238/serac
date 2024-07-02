79b671b75584fa2bbce9339a7e62fc4bc75b2da4
Chris White <white238@llnl.gov>
2024-07-02 15:42:31 -0700

    reduce debug statements, add info about branch, add mpi

--- .github/actions/apply-style/entrypoint.sh
+++ .github/actions/apply-style/entrypoint.sh
@@ -3,14 +3,21 @@
 CMAKE_ARGS=-DCMAKE_CXX_COMPILER=clang++
 CMAKE_ARGS="$CMAKE_ARGS -DENABLE_CLANGFORMAT=ON"
 CMAKE_ARGS="$CMAKE_ARGS -DCLANGFORMAT_EXECUTABLE=/usr/bin/clang-format"
+CMAKE_ARGS="$CMAKE_ARGS -DENABLE_MPI=ON"
+CMAKE_ARGS="$CMAKE_ARGS -DMPI_C_COMPILER=/usr/bin/mpicc"
+CMAKE_ARGS="$CMAKE_ARGS -DMPI_CXX_COMPILER=/usr/bin/mpic"
+CMAKE_ARGS="$CMAKE_ARGS -DMPI_Fortran_COMPILER=/usr/bin/mpif90"
 
 # Avoid error "fatal: detected dubious ownership in repository at '/github/workspace'"
 REPO_PATH=/github/workspace
 git config --global --add safe.directory "$REPO_PATH"
 find "$REPO_PATH" -type d | while read -r dir; do
-  echo "safe.directory: $dir"
   git config --global --add safe.directory "$dir"
 done
+
+echo "~~~~~~Branch~~~~~~~~"
+git branch
+echo "~~~~~~~~~~~~~~~~~~~~"
 
 git submodule update --init --recursive
 
