8af892afbf4f124b4adda240c8499b74a8cafb35
Chris White <white238@llnl.gov>
2024-07-02 17:55:52 -0700

    try tricking mpi #2

--- .github/actions/apply-style/entrypoint.sh
+++ .github/actions/apply-style/entrypoint.sh
@@ -4,9 +4,8 @@ CMAKE_ARGS=-DCMAKE_CXX_COMPILER=clang++
 CMAKE_ARGS="$CMAKE_ARGS -DENABLE_CLANGFORMAT=ON"
 CMAKE_ARGS="$CMAKE_ARGS -DCLANGFORMAT_EXECUTABLE=/usr/bin/clang-format"
 CMAKE_ARGS="$CMAKE_ARGS -DENABLE_MPI=ON -DENABLE_FIND_MPI=ON"
-# CMAKE_ARGS="$CMAKE_ARGS -DMPI_C_COMPILER=/usr/bin/mpicc"
-# CMAKE_ARGS="$CMAKE_ARGS -DMPI_CXX_COMPILER=/usr/bin/mpic++"
-# CMAKE_ARGS="$CMAKE_ARGS -DMPI_Fortran_COMPILER=/usr/bin/mpif90"
+CMAKE_ARGS="$CMAKE_ARGS -DMPI_C_COMPILER=/usr/bin/mpicc"
+CMAKE_ARGS="$CMAKE_ARGS -DMPI_CXX_COMPILER=/usr/bin/mpic++"
 
 # Avoid error "fatal: detected dubious ownership in repository at '/github/workspace'"
 REPO_PATH=/github/workspace
